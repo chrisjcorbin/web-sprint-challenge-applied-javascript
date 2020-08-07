4742ce5f45366538b4246cbfacd0014eda796c83
Chris Corbin <cjcorbin@gmail.com>
2020-08-07 10:57:16 -0700

    Carousel

--- components/Carousel.js
+++ components/Carousel.js
@@ -13,38 +13,50 @@
 */
 
 function carouselComponent() {
+  const imageFactory = {
+    0: "https://tk-assets.lambdaschool.com/ba687af4-3a1e-43d7-87b2-f30453264c9d_mountains.jpeg",
+    1: "https://tk-assets.lambdaschool.com/8aa075b0-67cf-47ce-9a7f-8cc9d754675d_computer.jpeg",
+    2: "https://tk-assets.lambdaschool.com/5b7441c6-6e4b-4feb-a4ec-8dd2eb76238a_trees.jpeg",
+    3: "https://tk-assets.lambdaschool.com/0b770382-d0eb-4465-8bf2-692a79fcda71_turntable.jpeg",
+  };
+
+  let carouselImageId = 0;
+
   const carousel = document.createElement("div");
   carousel.classList.add("carousel");
 
   const leftButton = document.createElement("div");
   leftButton.classList.add("left-button");
 
-  const imgContainer = document.createElement("div");
-  imgContainer.classList.add("img-container");
-
-  const mountainsImg = document.createElement("img");
-  mountainsImg.classList.add("mountainsImg");
-     mountainsImg.src = "https://tk-assets.lambdaschool.com/ba687af4-3a1e-43d7-87b2-f30453264c9d_mountains.jpeg";
-     imgContainer.appendChild(mountainsImg);
-
-  const computerImg = document.createElement("img");
-  computerImg.classList.add("computerImg");
-     computerImg.src = "https://tk-assets.lambdaschool.com/8aa075b0-67cf-47ce-9a7f-8cc9d754675d_computer.jpeg";
-     imgContainer.appendChild(computerImg);
-
-  const treesImg = document.createElement("img");
-  treesImg.classList.add("treesImg");
-     treesImg.src = "https://tk-assets.lambdaschool.com/5b7441c6-6e4b-4feb-a4ec-8dd2eb76238a_trees.jpeg";
-     imgContainer.appendChild(treesImg);
-
-  const turntableImg = document.createElement("img");
-  turntableImg.classList.add("turntableImg");
-     turntableImg.src = "https://tk-assets.lambdaschool.com/0b770382-d0eb-4465-8bf2-692a79fcda71_turntable.jpeg";
-     imgContainer.appendChild(turntableImg);
-
   const rightButton = document.createElement("div");
   rightButton.classList.add("right-button");
 
+  const imgContainer = document.createElement("img");
+
+  imgContainer.src = imageFactory[carouselImageId];
+  function next() {
+    if (carouselImageId === 3) {
+      carouselImageId = 0;
+    } else {
+      carouselImageId = carouselImageId + 1;
+    }
+    imgContainer.src = imageFactory[carouselImageId];
+  }
+
+  function previous() {
+    if (carouselImageId === 0) {
+      carouselImageId = 3;
+    } else {
+      carouselImageId = carouselImageId - 1;
+    }
+    imgContainer.src = imageFactory[carouselImageId];
+  }
+
+  
+  rightButton.onclick = next;
+  leftButton.onclick = previous;
+
+  carousel.append(leftButton, imgContainer, rightButton);
   return carousel;
 }
 
